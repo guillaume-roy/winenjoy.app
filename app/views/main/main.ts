c0d52b6072bb2efdc815183592d85bba21a98f00
Matthieu Bono <matthieu.bono@gmail.com>
2016-02-20 18:07:40 +0100

    Add floating button and related image

--- app/views/main/main.ts
+++ app/views/main/main.ts
@@ -2,6 +2,8 @@ import {EventData} from "data/observable";
 import {Page} from "ui/page";
 import {GestureEventData} from "ui/gestures";
 import {MainViewModel} from "../../view-models/main-view-model";
+import frameModule = require("ui/frame");
+import {Views} from "../../utils/views";
 
 let viewModel: MainViewModel;
 
@@ -13,4 +15,9 @@ export function navigatedTo(args: EventData) {
 
 export function onClick(args: GestureEventData) {
     viewModel.click();
+}
+
+export function fabTap(args) {
+    let topmost = frameModule.topmost();
+    topmost.navigate(Views.tasting);
 }
