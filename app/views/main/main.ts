5683d957f66bbd7b12bf7ec2c6b712c99043cad1
Guillaume ROY <gr.guillaume.roy@gmail.com>
2016-03-14 18:31:29 +0100

    Add navigation on FAB

--- app/views/main/main.ts
+++ app/views/main/main.ts
@@ -2,18 +2,44 @@ import {EventData} from "data/observable";
 import {Page} from "ui/page";
 import {MainViewModel} from "../../view-models/main-view-model";
 import frameModule = require("ui/frame");
+import {View} from "ui/core/view";
 import {Views} from "../../utils/views";
 
 let viewModel: MainViewModel;
-let topMost: any;
 
 export function navigatedTo(args: EventData) {
     let page = <Page>args.object;
     viewModel = new MainViewModel();
     page.bindingContext = viewModel;
-    topMost = frameModule.topmost();
 }
 
-export function fabTap(args) {
-    topMost.navigate(Views.tasting);
+export function onCreateNewTasting(args: EventData) {
+    let button = <View>args.object;
+    button.icon = "res://blank";
+    button.animate({
+       duration: 300,
+       scale: {
+           x: 20,
+           y: 20
+       },
+       translate: {
+           x: -200,
+           y: -200
+       }
+    }).then(v => {
+        frameModule.topmost().navigate({
+            animated: false,
+            moduleName: Views.tasting,
+            transition: null
+        });
+
+        button.animate({ // reset to original state
+            delay: 300,
+            duration: 0,
+            scale: { x: 1, y: 1 },
+            translate: { x: 0, y: 0 },
+        }).then(function(){
+            button.icon = "res://ic_add_white_24dp";
+        });
+    });
 }
