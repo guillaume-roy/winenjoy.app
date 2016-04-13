0a8b39c17572ce455bd18912891cf90199e483cc
Guillaume ROY <gr.guillaume.roy@gmail.com>
2016-04-13 15:26:44 +0200

    Update beta restriction

--- app/view-models/main-view-model.ts
+++ app/view-models/main-view-model.ts
@@ -27,7 +27,7 @@ export class MainViewModel extends Observable {
     constructor() {
         super();
 
-        this.betaIsClosed = new Date().getTime() > new Date(2016, 3, 17).getTime();
+        this.betaIsClosed = new Date().getTime() > new Date(2016, 4, 1).getTime();
 
         this._service = new TastingsService();
 
